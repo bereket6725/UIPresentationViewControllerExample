304bf9b68c1a947355a1b1f9b8e4ee6e92150587
bereket6725 <bereket6725@gmail.com>
2018-08-10 12:13:12 -0400

    extended SlideInPresentationManager to implement UIViewControllerAnimatedTransitioning delegate methods

--- MedalCount Starter/MedalCount/SlideInPresentationAnimator.swift
+++ MedalCount Starter/MedalCount/SlideInPresentationAnimator.swift
@@ -22,3 +22,14 @@ final class SlideInPresentationAnimator: NSObject {
     super.init()
   }
 }
+
+extension SlideInPresentationAnimator: UIViewControllerAnimatedTransitioning {
+  
+  func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?)-> TimeInterval {
+    return 0.3
+  }
+  
+  func animateTransition(using transitionContext: UIViewControllerContextTransitioning){
+  }
+  
+}
