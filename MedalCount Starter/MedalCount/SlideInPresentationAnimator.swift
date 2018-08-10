67bcdb20fc4b6237abe382437f4b3974761267da
bereket6725 <bereket6725@gmail.com>
2018-08-10 12:50:02 -0400

    implemented logic in the animateTransition function to calculate the frames our custom presentation is animating from and to and execute custom animation

--- MedalCount Starter/MedalCount/SlideInPresentationAnimator.swift
+++ MedalCount Starter/MedalCount/SlideInPresentationAnimator.swift
@@ -24,12 +24,48 @@ final class SlideInPresentationAnimator: NSObject {
 }
 
 extension SlideInPresentationAnimator: UIViewControllerAnimatedTransitioning {
-  
+  //defines length of transition
   func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?)-> TimeInterval {
     return 0.3
   }
-  
+  //performs animations
+  //defines how long the transition takes
+  //1. if this is a presentation, the method asks the transitionContext for the viewController assoicated with the '.to' key (the viewController you are moving to). If its dismissal, it asks the transitionContext for the viewController associate with the .from  key (he viewController you are moving from)
+  //2. if the action is a presentation, we add the view Controllers view to the view hierarchy; this part uses the transition context to get the container view
+  //3. Calculate the frames you are animating 'from' and 'to'
+  //   the first line in part 3 asks the transitionContext for the views frame when its presented. The rest calculates its frame when its dismissed. This section sets the frames origin so its just outside the visible area based on the frames direction
+  //4. Determines the transitiona inital and final frames. When presenting the viewController it from the dimissed to the presented frames and vice-versa for when its dismissing
   func animateTransition(using transitionContext: UIViewControllerContextTransitioning){
+    //1.
+    let key = isPresentation ? UITransitionContextViewControllerKey.to : UITransitionContextViewControllerKey.from
+    let controller = transitionContext.viewController(forKey: key)!
+    //2
+    if isPresentation {
+      transitionContext.containerView.addSubview(controller.view)
+    }
+    //3
+    let presentedFrame = transitionContext.finalFrame(for: controller)
+    var dismissedFrame = presentedFrame
+    switch direction {
+    case .left:
+      dismissedFrame.origin.x = -presentedFrame.width
+    case .right:
+      dismissedFrame.origin.x = transitionContext.containerView.frame.size.width
+    case .top:
+      dismissedFrame.origin.y = -presentedFrame.height
+    case .bottom:
+      dismissedFrame.origin.y = transitionContext.containerView.frame.size.height
+    }
+    //4
+    let initialFrame = isPresentation ? dismissedFrame : presentedFrame
+    let finalFrame = isPresentation ? presentedFrame : dismissedFrame
+    //5
+    let animationDuration = transitionDuration(using: transitionContext)
+    controller.view.frame = initialFrame
+    UIView.animate(withDuration: animationDuration, animations: {
+      controller.view.frame = finalFrame}) {finished in
+        transitionContext.completeTransition(finished)
+    }
   }
   
 }
